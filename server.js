9745007b10c5609dcbdb15789863339c322769c2
itsmtt <ithelpdesk.smtt@gmail.com>
2025-02-05 22:11:00 +0700

    update authenticateToken, in server

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.post("/add-contact", authenticateToken, (req, res) => {
 });
 
 // Endpoint untuk mendapatkan daftar kontak dengan pagination
-app.get("/get-contacts", (req, res) => {
+app.get("/get-contacts", authenticateToken, (req, res) => {
   const { page = 1, limit = 5 } = req.query;
   const pageNumber = parseInt(page, 10);
   const limitNumber = parseInt(limit, 10);
@@ -237,7 +237,7 @@ app.post("/schedule-reminder", authenticateToken, (req, res) => {
 });
 
 // Endpoint untuk mendapatkan daftar pengingat dengan pagination
-app.get("/get-reminders", (req, res) => {
+app.get("/get-reminders", authenticateToken, (req, res) => {
   const { page = 1, limit = 5 } = req.query;
   const pageNumber = parseInt(page, 10);
   const limitNumber = parseInt(limit, 10);
@@ -310,7 +310,7 @@ app.delete("/delete-reminder/:id", authenticateToken, (req, res) => {
 });
 
 // Endpoint untuk mendapatkan daftar pengingat terkirim dengan pagination
-app.get("/get-sent-reminders", (req, res) => {
+app.get("/get-sent-reminders",authenticateToken, (req, res) => {
   const { page = 1, limit = 5 } = req.query;
   const pageNumber = parseInt(page, 10);
   const limitNumber = parseInt(limit, 10);
