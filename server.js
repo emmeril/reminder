0a9e5a6e7d9da9db6ae517a7d8446db27d716db1
itsmtt <ithelpdesk.smtt@gmail.com>
2025-02-12 19:52:25 +0700

    Remove redundant assignment of reminder ID in rescheduleReminderToNextMonth function

--- server.js
+++ server.js
@@ -465,7 +465,6 @@ const rescheduleReminderToNextMonth = (reminder) => {
     .replace(/\d{4}-\d{2}-\d{2}/, formattedDate); // Ganti tanggal
 
   // Update reminder date and message
-  reminder.id = nextMonthDate;
   reminder.phoneNumber;
   reminder.reminderDateTime = nextMonthDate;
   reminder.message = updatedMessage;
